14b0d0a79304cdb570c1e5893d071a61647e60da
johnhury <johnhery@126.com>
2016-05-12 11:38:23 +0800

    2rd commit

---
+++ 30/js/myApp.js
@@ -0,0 +1,11 @@
+﻿var myApp=angular.module('myApp',[]);//定义一个模块
+myApp.controller('UserController',function($scope){
+	var user={};
+	$scope.user=user;
+	user.name="johnhery";
+	user.email="john@lvdot.com";
+	user.subscribe=function(){
+		
+		console.log(user.name+"您订阅的邮件已经发送到您的邮箱"+user.email+"请查收");
+	}
+});
